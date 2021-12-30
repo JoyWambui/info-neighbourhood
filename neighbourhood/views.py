e1e3d6aa14cf7a3721f4d487ab6ac7d374929e6a
JoyWambui <joywambuindegwa@gmail.com>
2021-12-30 20:53:03 +0300

    add database configuration

--- neighbourhood/views.py
+++ neighbourhood/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
 
+def register(request):
+    '''View function to register new users'''
+
+def landing(request):
+    return render(request,'landing.html')
+    
+
 def homepage(request):
     return render(request,'homepage.html')
