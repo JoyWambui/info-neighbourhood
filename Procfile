e1aa936cc8c5332c25483949eab8f7979909e8c7
JoyWambui <joywambuindegwa@gmail.com>
2022-01-08 18:29:40 +0300

    create neighbourhood model and its methods

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
+release: python3.9 manage.py migrate
 web: gunicorn info_neighbourhood.wsgi --log-file -
--- neighbourhood/models.py
+++ neighbourhood/models.py
@@ -1,4 +1,53 @@
+from os import name
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import RegexValidator
+from django.http import Http404
 
+
+class NeighbourHood(models.Model):
+    name = models.CharField(max_length=50)
+    location = models.CharField(max_length=50)
+    occupants = models.IntegerField()
+    phoneNumberRegex = RegexValidator(regex=r"^\+?1?\d{8,15}$")
+    health_dept = models.CharField(validators=[phoneNumberRegex],max_length=16,unique=True)
+    police_dept = models.CharField(validators=[phoneNumberRegex],max_length=16,unique=True)
+    admin = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+    def create_neighbourhood(self):
+        self.save()
+    
+    @classmethod
+    def get_neighbourhoods(cls):
+        return cls.objects.all()
+    
+    @classmethod
+    def find_neighbourhood(cls,neighbourhood_id):
+        try:
+            return cls.objects.get(pk=neighbourhood_id)
+        except NeighbourHood.DoesNotExist:
+            return Http404
+    
+    @classmethod    
+    def update_neighbourhood(cls,neighbourhood_id,new_name):
+        return NeighbourHood.objects.filter(id=neighbourhood_id).update(name=new_name)
+    
+    @classmethod    
+    def update_occupants(cls,neighbourhood_id,occupant_count):
+        return NeighbourHood.objects.filter(id=neighbourhood_id).update(occupants=occupant_count)
+    
+    @classmethod    
+    def delete_neighbourhood(cls,neighbourhood_id):
+        return NeighbourHood.objects.filter(id=neighbourhood_id).delete()
+    
+    def __str__(self):
+        return self.name
 class Profile(models.Model):
+    pass
+
+
+class Post(models.Model):
+    pass
+
+class Business(models.Model):
     pass
